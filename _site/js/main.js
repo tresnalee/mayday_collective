205a7c5d493d9155f0174de88fa9871fbfd3a63c
tresnalee <tresna@Tresnas-MacBook-Air.local>
2017-03-11 10:01:44 +0800

    Style hamburger

--- _site/js/main.js
+++ _site/js/main.js
@@ -1,5 +1,21 @@
 $(document).ready(function(){
 
-  // jquery goes here
+  $( ".cross" ).hide();
+
+  $( ".menu" ).hide();
+
+  $( ".hamburger" ).click(function() {
+    $( ".menu" ).slideToggle( "slow", function() {
+      $( ".hamburger" ).hide();
+      $( ".cross" ).show();
+    });
+  });
+
+  $( ".cross" ).click(function() {
+    $( ".menu" ).slideToggle( "slow", function() {
+      $( ".cross" ).hide();
+      $( ".hamburger" ).show();
+    });
+  });
 
 });
